05e94f0119fc7bf0d5c8b31131a6fdeccecb3718
rlyhan <richard_han@live.com>
2020-01-27 23:38:45 +1300

    Added projects and contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,19 @@ import React, { forwardRef } from 'react'
 const Contact = forwardRef((props, ref) => {
   return (
     <div className="contact" ref={ref}>
-      <div>
+      <div className="page-content">
+        <h3>EMAIL</h3>
+        <p>richard.ly.han@gmail.com</p>
+        <h3>PHONE</h3>
+        <p>021 025 18876</p>
+        <div className="contact-icons">
+          <a href="https://github.com/rlyhan" target="_blank">
+            <img src={require('../images/github.png')} alt="github" style={{width: "calc(100% - 8px)"}} />
+          </a>
+          <a href="https://www.linkedin.com/in/richard-ly-han/" target="_blank">
+            <img src={require('../images/linkedin.svg')} alt="linkedin" />
+          </a>
+        </div>
       </div>
     </div>
   )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,29 @@ import React, { forwardRef } from 'react'
 const Projects = forwardRef((props, ref) => {
   return (
     <div className="projects" ref={ref}>
-      <div>
+      <div className="page-content">
+        <div className="project">
+          <a href="https://explore-welly.herokuapp.com/" target="_blank">
+            <img src={require('../images/wellington-day.jpg')} />
+          </a>
+          <h2><strong>WELLY.</strong></h2>
+          <p>
+            Personal project. Site used to browse through shops, restaurants,
+            bars and more in the city of Wellington, New Zealand.
+          </p>
+        </div>
+        <div className="project">
+          <a href="https://foraged-and-found.herokuapp.com/" target="_blank">
+            <img src={require('../images/wellington-day.jpg')} />
+          </a>
+          <h2><strong>FORAGED AND FOUND</strong></h2>
+          <p>
+            Final group project at Enspiral Dev Academy. An application for the
+            foraging community, allowing them to easily locate foragable items
+            (like fruits and herbs) on a Google Map and also add items for others
+            to find or for their own reference.
+          </p>
+        </div>
       </div>
     </div>
   )
