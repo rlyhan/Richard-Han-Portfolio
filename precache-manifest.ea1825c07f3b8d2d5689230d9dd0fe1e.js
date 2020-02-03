7c4d05833359e5f5f3f11e129178d6fa805564c4
rlyhan <richard_han@live.com>
2020-02-03 20:03:21 +1300

    Updates

---
+++ precache-manifest.ea1825c07f3b8d2d5689230d9dd0fe1e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f167e1d9c6a893ca6ea362837a7f26",
+    "url": "/Richard-Han-Portfolio/index.html"
+  },
+  {
+    "revision": "819b4ce2be7675233caf",
+    "url": "/Richard-Han-Portfolio/static/js/2.511d4f4f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Richard-Han-Portfolio/static/js/2.511d4f4f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b783e2e30322d0b2b94",
+    "url": "/Richard-Han-Portfolio/static/js/main.3e0bd3ba.chunk.js"
+  },
+  {
+    "revision": "7260094566b40a027f44",
+    "url": "/Richard-Han-Portfolio/static/js/runtime-main.5d98f412.js"
+  },
+  {
+    "revision": "cdc79732978b45dc04582c47e33a3263",
+    "url": "/Richard-Han-Portfolio/static/media/Richard_Han_CV.cdc79732.pdf"
+  },
+  {
+    "revision": "959efd9fc0957df7625f75f9af8558b9",
+    "url": "/Richard-Han-Portfolio/static/media/foraging.959efd9f.jpg"
+  },
+  {
+    "revision": "6d9df42c29a271ab2a21944310f6820a",
+    "url": "/Richard-Han-Portfolio/static/media/linkedin.6d9df42c.svg"
+  },
+  {
+    "revision": "46b6be71c4d589608d31147a1eabae19",
+    "url": "/Richard-Han-Portfolio/static/media/wellington-day.46b6be71.jpg"
+  }
+]);
