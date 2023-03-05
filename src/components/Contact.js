902a9fc9848faad2f9ccb39aad76ba919c9d9129
richard han <richard_han@live.com>
2023-03-05 13:48:02 +1300

    Style tweaks

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -41,6 +41,25 @@ const Contact = () => {
             <span>@gmail.com</span>
           </a>
         </div>
+        <div className="footer">
+          <p className="copyright">All Rights Reserved</p>
+          <div className="contact-links">
+            <a
+              href="https://www.linkedin.com/in/richard-ly-han/"
+              target="_blank"
+              className="linkedin"
+            >
+              <span>LinkedIn</span>
+            </a>
+            <a
+              href="https://github.com/rlyhan"
+              target="_blank"
+              className="github"
+            >
+              <span>GitHub</span>
+            </a>{" "}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/MainPages.js
+++ src/components/MainPages.js
@@ -11,25 +11,6 @@ function MainPages({ currentPage, setCurrentPage }) {
       <About />
       <Projects />
       <Contact />
-      <div className="footer">
-        <p className="copyright">All Rights Reserved</p>
-        <div className="contact-links">
-          <a
-            href="https://www.linkedin.com/in/richard-ly-han/"
-            target="_blank"
-            className="linkedin"
-          >
-            <span>LinkedIn</span>
-          </a>
-          <a
-            href="https://github.com/rlyhan"
-            target="_blank"
-            className="github"
-          >
-            <span>GitHub</span>
-          </a>{" "}
-        </div>
-      </div>
     </>
   );
 }
